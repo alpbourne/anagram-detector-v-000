f57b11ffb57e2f7007cf80013590157d0cb6fc49
alpbourne <alpbourne@gmail.com>
2017-09-27 20:58:07 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,9 +8,9 @@ class Anagram
 
   def match(array_of_words)
     array = []
-    array_of_words.select do |a|
-      if a.split("").sort == @word.split("").sort
-        array << a
+    array_of_words.select do |anagram|
+      if anagram.split("").sort == @word.split("").sort
+        array << anagram
       else
         empty_array
       end
