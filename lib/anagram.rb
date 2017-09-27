bf10c30a3d27b0abb540cac3eb9daa554c39c9a3
alpbourne <alpbourne@gmail.com>
2017-09-27 21:07:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,8 +7,8 @@ class Anagram
   end
 
   def match(array_of_words)
-    array_of_words.select do |anagrams|
-      anagrams.split("").sort == @word.split("").sort
+    array_of_words.select do |possible_anagrams|
+      possible_anagrams.split("").sort == @word.split("").sort
     end
   end
 
