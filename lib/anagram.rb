9eeefe0da32e50cb65165c99d3e8fe8cf0ac1b5b
alpbourne <alpbourne@gmail.com>
2017-09-27 20:36:17 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,13 @@ class Anagram
   end
 
   def match(array_of_words)
-    possible_anagrams = %w(array_of_words)
-
+    empty_array = []
+    @word.each do |array_of_words|
+      if @word.split("").sort == array_of_words.split("").sort
+        array_of_words
+      else
+        empty_array  
+      end
   end
 
 end
